48d516f718b792ab4e72e525c1dc634e2fc7f0b7
melanie basnak <melaniebasnak@g.harvard.edu>
2021-09-28 18:12:31 -0400

    added bump width to the analysis

--- Uncertainty/Exp30/source/change_in_bm_across_flies.m
+++ Uncertainty/Exp30/source/change_in_bm_across_flies.m
@@ -5,26 +5,44 @@ clear all; close all
 folderNames = dir('Z:\Wilson Lab\Mel\Experiments\Uncertainty\Exp30\data\pilots\high_contrast');
 
 change_bm = [];
+change_bw = [];
 
 for folder = 1:length(folderNames)
    if contains(folderNames(folder).name,'60D05') == 1
-       load(fullfile(folderNames(folder).folder,folderNames(folder).name,'analysis\bm_change.mat'))
+       load(fullfile(folderNames(folder).folder,folderNames(folder).name,'analysis\bump_parameter_change.mat'))
        mean_bm_aj = mean(bm_aj,2);
+       mean_bw_aj = mean(bw_aj,2);
        change_bm = [change_bm,mean_bm_aj];
+       change_bw = [change_bw,mean_bw_aj];
    end
 end
 
 path = 'Z:\Wilson Lab\Mel\Experiments\Uncertainty\Exp30\data\pilots\high_contrast\group_plots';
 
-%% Plot
+%% Plot BM
 
 figure,
 plot(change_bm,'-o','color',[.5 .5 .5])
 hold on
 plot(mean(change_bm,2),'-ko','linewidth',2)
 xlim([0 3]);
-ylim([0.5 1.3]);
+ylim([min(min(change_bm))-0.5 max(max(change_bm))+0.5]);
 xticks([1 2])
 xticklabels({'pre-jump','post-jump'})
 ylabel('Mean bump magnitude (max-min)');
 saveas(gcf,[path,'\change_in_bm_AJ.png'])
+
+
+%% Plot BW
+
+figure,
+plot(change_bw,'-o','color',[.5 .5 .5])
+hold on
+plot(mean(change_bw,2),'-ko','linewidth',2)
+xlim([0 3]);
+ylim([min(min(change_bw))-0.5 max(max(change_bw))+0.5]);
+xticks([1 2])
+xticklabels({'pre-jump','post-jump'})
+ylabel('Mean bump width');
+saveas(gcf,[path,'\change_in_bw_AJ.png'])
+
