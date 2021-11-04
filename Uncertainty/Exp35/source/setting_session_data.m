02f4d89905bad249462e9ff0553bcca3a679255e
melanie basnak <melaniebasnak@g.harvard.edu>
2021-11-04 17:44:38 -0400

    made code to generate the session info data

---
+++ Uncertainty/Exp35/source/setting_session_data.m
@@ -0,0 +1,10 @@
+sessions = struct();
+
+sessions.initial_cl_bar = 2;
+sessions.initial_cl_wind = 1;
+sessions.cue_combination= 3;
+sessions.final_cl_bar = 5;
+sessions.final_cl_wind = 4;
+
+
+save('Z:\Wilson Lab\Mel\Experiments\Uncertainty\Exp35\data\high_reliability\20211102_60D05_7f\analysis\sessions_info.mat','sessions')
