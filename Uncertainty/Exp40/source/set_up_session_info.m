e5d325b25c492fd1f8871903207fc2aebd198b7a
melanie basnak <melaniebasnak@g.harvard.edu>
2022-02-23 17:51:26 -0500

    Made code to save session info for the offset control experiment

---
+++ Uncertainty/Exp40/source/set_up_session_info.m
@@ -0,0 +1,9 @@
+%code to set session info
+
+
+session_info = struct();
+session_info.empty = 1;
+session_info.wind = 0;
+session_info.bar = 2;
+
+save('Z:\Wilson Lab\Mel\Experiments\Uncertainty\Exp40\data\20220216_60D05_7f_fly2\analysis\session_info.mat')
